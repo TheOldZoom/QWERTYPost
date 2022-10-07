2a9157dc8bab78093b650109eec5d064b0f62671
xabou2010b <xabou2010@gmail.com>
2022-10-07 12:58:30 -0600

    Création de la database

---
+++ actions/database.php
@@ -0,0 +1,14 @@
+<?php
+
+try{
+    $bdd = new PDO('mysql:host=localhost;dbname=connectbd;charset=utf8;', 'root', '');
+}   catch(PDOException $e){
+    die('Une erreur a été trouvée : ' . $e->getMessage());
+}   echo'Connexion reussi';
+
+
+?>
+
+
+
+
